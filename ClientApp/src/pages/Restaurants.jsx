5b0f50e1af09c3bb332eaf8390e6b6d18a12fe28
Gavin Stark <gavin@gstark.com>
2020-07-05 16:07:51 -0400

    Renders restaurants from empty state

--- ClientApp/src/pages/Restaurants.jsx
+++ ClientApp/src/pages/Restaurants.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export function Restaurants() {
+  const [restaurants, setRestaurants] = useState([])
+
   return (
     <>
       <nav aria-label="breadcrumb">
@@ -10,58 +12,40 @@ export function Restaurants() {
             <Link to="#">Home</Link>
           </li>
           <li className="breadcrumb-item active" aria-current="page">
-            2 Taco Joints Found
+            {restaurants.length} Taco Joints Found
           </li>
         </ol>
       </nav>
       <div className="list-group">
-        <Link to="/tacos/42" className="list-group-item list-group-item-action">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">Taco Heaven</h5>
-            <small>5 Reviews</small>
-          </div>
-          <p className="mb-1">123 Main Street</p>
-          <small className="mr-3">
-            <button className="btn btn-success btn-sm">
-              <span className="mr-2" role="img" aria-label="upvote">
-                👍🏻
-              </span>
-              5
-            </button>
-          </small>
-          <small className="mr-3">
-            <button className="btn btn-danger btn-sm">
-              <span className="mr-2" role="img" aria-label="downvote">
-                👎🏻
-              </span>{' '}
-              3
-            </button>
-          </small>
-        </Link>
-
-        <Link to="/tacos/42" className="list-group-item list-group-item-action">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">Taco Mania</h5>
-            <small>2 Reviews</small>
-          </div>
-          <p className="mb-1">555 East Pima</p>
-          <small className="mr-3">
-            <button className="btn btn-success btn-sm">
-              <span className="mr-2" role="img" aria-label="upvote">
-                👍🏻
-              </span>
-              10
-            </button>
-          </small>
-          <small className="mr-3">
-            <button className="btn btn-danger btn-sm">
-              <span className="mr-2" role="img" aria-label="downvote">
-                👎🏻
-              </span>{' '}
-              1
-            </button>
-          </small>
-        </Link>
+        {restaurants.map(restaurant => (
+          <Link
+            key={restaurant.id}
+            to={`/tacos/${restaurant.id}`}
+            className="list-group-item list-group-item-action"
+          >
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">{restaurant.name}</h5>
+              <small>5 Reviews</small>
+            </div>
+            <p className="mb-1">{restaurant.address}</p>
+            <small className="mr-3">
+              <button className="btn btn-success btn-sm">
+                <span className="mr-2" role="img" aria-label="upvote">
+                  👍🏻
+                </span>
+                5
+              </button>
+            </small>
+            <small className="mr-3">
+              <button className="btn btn-danger btn-sm">
+                <span className="mr-2" role="img" aria-label="downvote">
+                  👎🏻
+                </span>{' '}
+                3
+              </button>
+            </small>
+          </Link>
+        ))}
       </div>
     </>
   )
