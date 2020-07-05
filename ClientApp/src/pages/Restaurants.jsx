6500b761dc09d9064af4529621f6fb937f9f7dd8
Gavin Stark <gavin@gstark.com>
2020-07-05 16:14:13 -0400

    Created component for a single restaurant in a list

--- ClientApp/src/pages/Restaurants.jsx
+++ ClientApp/src/pages/Restaurants.jsx
@@ -1,6 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+function SingleRestaurantFromList(props) {
+  const restaurant = props.restaurant
+
+  return (
+    <Link
+      to={`/tacos/${restaurant.id}`}
+      className="list-group-item list-group-item-action"
+    >
+      <div className="d-flex w-100 justify-content-between">
+        <h5 className="mb-1">{restaurant.name}</h5>
+        <small>5 Reviews</small>
+      </div>
+      <p className="mb-1">{restaurant.address}</p>
+      <small className="mr-3">
+        <button className="btn btn-success btn-sm">
+          <span className="mr-2" role="img" aria-label="upvote">
+            👍🏻
+          </span>
+          5
+        </button>
+      </small>
+      <small className="mr-3">
+        <button className="btn btn-danger btn-sm">
+          <span className="mr-2" role="img" aria-label="downvote">
+            👎🏻
+          </span>{' '}
+          3
+        </button>
+      </small>
+    </Link>
+  )
+}
+
 export function Restaurants() {
   const [restaurants, setRestaurants] = useState([])
 
@@ -26,33 +59,10 @@ export function Restaurants() {
       </nav>
       <div className="list-group">
         {restaurants.map(restaurant => (
-          <Link
+          <SingleRestaurantFromList
             key={restaurant.id}
-            to={`/tacos/${restaurant.id}`}
-            className="list-group-item list-group-item-action"
-          >
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">{restaurant.name}</h5>
-              <small>5 Reviews</small>
-            </div>
-            <p className="mb-1">{restaurant.address}</p>
-            <small className="mr-3">
-              <button className="btn btn-success btn-sm">
-                <span className="mr-2" role="img" aria-label="upvote">
-                  👍🏻
-                </span>
-                5
-              </button>
-            </small>
-            <small className="mr-3">
-              <button className="btn btn-danger btn-sm">
-                <span className="mr-2" role="img" aria-label="downvote">
-                  👎🏻
-                </span>{' '}
-                3
-              </button>
-            </small>
-          </Link>
+            restaurant={restaurant}
+          />
         ))}
       </div>
     </>
