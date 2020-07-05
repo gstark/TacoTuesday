ac137ad972bcbb7732807096bf845dba3db1a391
Gavin Stark <gavin@gstark.com>
2020-07-05 15:42:01 -0400

    Initial static design

---
+++ ClientApp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export function Header() {
+  return (
+    <>
+      <h1>Welcome to Taco Tuesday.</h1>
+      <h2 className="mb-4">Your home for great tacos!</h2>
+    </>
+  )
+}
