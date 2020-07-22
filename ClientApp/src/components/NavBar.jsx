e100facc11ef66f62639e99154e279e73c1cdfd8
Gavin Stark <gavin@gstark.com>
2020-07-22 08:55:27 -0400

    Makes navbar reveal/hide work

--- ClientApp/src/components/NavBar.jsx
+++ ClientApp/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { Route } from 'react-router'
 import { isLoggedIn, logout } from '../pages/auth'
 
 export function NavBar(props) {
+  const [navbarIsOpen, setNavbarIsOpen] = useState(false)
   const [filterText, setFilterText] = useState('')
 
   const handleClickSearch = () => {
@@ -28,16 +29,16 @@ export function NavBar(props) {
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
         aria-label="Toggle navigation"
+        onClick={() => setNavbarIsOpen(!navbarIsOpen)}
       >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div
+        className={`collapse navbar-collapse ${navbarIsOpen ? 'show' : ''}`}
+        id="navbarSupportedContent"
+      >
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <Link className="nav-link" to="/">
