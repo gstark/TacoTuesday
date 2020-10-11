5a9081b64a4c203d34df4ba9b2689e2c13908d8f
Gavin Stark <gavin@gstark.com>
2020-10-11 11:38:15 -0400

    Add support for reviews

--- ClientApp/src/pages/Restaurants.jsx
+++ ClientApp/src/pages/Restaurants.jsx
@@ -18,7 +18,7 @@ function SingleRestaurantFromList(props) {
           style={{ '--rating': 4.7 }}
           aria-label="Star rating of this location is 4.7 out of 5."
         ></span>
-        (2,188)
+        ({props.restaurant.reviews.length})
       </p>
       <address>{props.restaurant.address}</address>
     </li>
