ef38b498b4af2f96ac67426c3f2c0c7994a00553
Gavin Stark <gavin@gstark.com>
2020-10-11 14:45:26 -0400

    Creates a layout in the App

--- ClientApp/src/pages/Restaurants.jsx
+++ ClientApp/src/pages/Restaurants.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import avatar from '../images/avatar.png'
 import tacoTuesday from '../images/taco-tuesday.svg'
 import map from '../images/map.png'
 
@@ -46,55 +45,33 @@ export function Restaurants() {
   }, [filterText])
 
   return (
-    <>
-      <header>
-        <ul>
-          <li>
-            <nav>
-              <Link to="/new">
-                <i className="fa fa-plus"></i> Restaurant
-              </Link>
-              <p>Welcome back, Steve!</p>
-            </nav>
-          </li>
-          <li className="avatar">
-            <img src={avatar} alt="Steve's Avatar" height="64" width="64" />
-          </li>
-        </ul>
-      </header>
-      <main className="home">
-        <h1>
-          <img src={tacoTuesday} alt="Taco Tuesday" />
-        </h1>
-        <form className="search">
-          <input
-            type="text"
-            placeholder="Search..."
-            value={filterText}
-            onChange={function (event) {
-              setFilterText(event.target.value)
-            }}
+    <main className="home">
+      <h1>
+        <img src={tacoTuesday} alt="Taco Tuesday" />
+      </h1>
+      <form className="search">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={function (event) {
+            setFilterText(event.target.value)
+          }}
+        />
+      </form>
+
+      <section className="map">
+        <img alt="Example Map" src={map} />
+      </section>
+
+      <ul className="results">
+        {restaurants.map((restaurant) => (
+          <SingleRestaurantFromList
+            key={restaurant.id}
+            restaurant={restaurant}
           />
-        </form>
-
-        <section className="map">
-          <img alt="Example Map" src={map} />
-        </section>
-
-        <ul className="results">
-          {restaurants.map((restaurant) => (
-            <SingleRestaurantFromList
-              key={restaurant.id}
-              restaurant={restaurant}
-            />
-          ))}
-        </ul>
-      </main>
-      <footer>
-        <p>
-          Built with <i className="fa fa-heart"></i> in St Petersburg, Florida.
-        </p>
-      </footer>
-    </>
+        ))}
+      </ul>
+    </main>
   )
 }
